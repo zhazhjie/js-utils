6f6666579df269ad86b5e25f62a661e9d79fda50
zhazhjie <583714409@qq.com>
2020-04-22 22:10:34 +0800

    +1

--- validate.js
+++ validate.js
@@ -5,20 +5,40 @@
  * @version: 1.0
  */
 export class Validate {
+  /**
+   * 验证手机号
+   * @param phone {String}
+   * @return {boolean}
+   */
   static isPhone(phone) {
     let reg = /^1[23456789]\d{9}$/;
     return reg.test(phone);
   }
 
+  /**
+   * 验证邮箱
+   * @param email {String}
+   * @return {boolean}
+   */
   static isEmail(email) {
     let reg = /^[A-Za-z0-9]+([_\.][A-Za-z0-9]+)*@([A-Za-z0-9\-]+\.)+[A-Za-z]{2,6}$/;
     return reg.test(email);
   }
 
+  /**
+   * 判空
+   * @param value {*}
+   * @return {boolean}
+   */
   static isEmpty(value) {
     return value === null || value === undefined || value === "";
   }
 
+  /**
+   * 是否为对象
+   * @param value {*}
+   * @return {boolean}
+   */
   static isObject(value) {
     return Object.prototype.toString.call(value) === "[object Object]";
   }
